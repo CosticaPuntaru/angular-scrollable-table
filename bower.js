aa64b1d3e8c6d2c2ee28fae7194f9e15d79368c4
CosticaPuntaru <ticaasd@gmail.com>
2014-11-19 14:08:04 +0200

    Create bower.js

---
+++ bower.js
@@ -0,0 +1,21 @@
+{
+  "name": "pc-angular-fixed-header-table",
+  "version": "1.1.0",
+  "description": "AngularJS fixed header scrollable table directive",
+  "main": "angu-fixed-header-table.js",
+  "keywords": [
+    "AngularJS",
+    "Fixed Header Table"
+  ],
+  "license": "MIT",
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test",
+    "tests"
+  ],
+  "dependencies": {
+    "angular": ">=1.2.16"
+  }
+}
